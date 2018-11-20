b44250c5fd608cfb030ea72e963612d2eeda3e3a
Matthew Plavcan <mplavcan@vrmotioncorp.com>
2018-11-19 23:27:00 -0800

    Added test to demonstrate that List is wrong implementation

--- Assets/Unit Tests/Editor/GridTests.cs
+++ Assets/Unit Tests/Editor/GridTests.cs
@@ -35,12 +35,20 @@ public class GridTests
         Assert.That(grid.CellStateAt(Vector2.one), Is.EqualTo(CellState.Alive));
     }
 
-
     [Test]
     public void GridCellStateQueryIsDistinct()
     {
         var grid = new GameGrid();
         grid.AddLivingCell(Vector2.zero);
         Assert.That(grid.CellStateAt(Vector2.one), Is.EqualTo(CellState.Dead));
+    }
+
+    [Test]
+    public void LivingCellCoordinatesAreUnique()
+    {
+        var grid = new GameGrid();
+        grid.AddLivingCell(Vector2.one);
+        grid.AddLivingCell(Vector2.one);
+        Assert.That(grid.CellCount(), Is.EqualTo(1));
     }
 }
