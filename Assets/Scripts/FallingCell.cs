7f1806f2ccb5b651779beedfb9834c7977dd7559
Matthew Plavcan <mplavcan@vrmotioncorp.com>
2018-11-20 01:15:26 -0800

    Added color fading to cell Changed cell shape to sphere

--- Assets/Scripts/FallingCell.cs
+++ Assets/Scripts/FallingCell.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class FallingCell : MonoBehaviour
 {
+    [SerializeField] private Color color;
+
     private const int VanishingThreshold = -50;
+    private GameObject cube;
+
+    void Start()
+    {
+        FadeColorWithDepth();
+    }
 
     void Update()
     {
+        FadeColorWithDepth();
         RemoveFallenCell();
+    }
+
+    private void FadeColorWithDepth()
+    {
+        GetComponent<MeshRenderer>().material.color =
+            color * (1 - (transform.position.y / VanishingThreshold));
     }
 
     private void RemoveFallenCell()
