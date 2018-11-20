a68ec2b44805d80f4d9609a70947699f7fffc257
Matthew Plavcan <mplavcan@vrmotioncorp.com>
2018-11-20 00:46:34 -0800

    Added grid animation monbehaviour

---
+++ Assets/Scripts/LifeView.cs
@@ -0,0 +1,39 @@
+﻿// 
+
+﻿using System.Collections;
+using GameOfLife;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class LifeView : MonoBehaviour
+{
+    private bool running = true;
+    private const float TimeStep = .5f;
+    private GameGrid grid = new GameGrid();
+
+    private void Start()
+    {
+        InputTracking.Recenter();
+        StartCoroutine(LifeAnimator());
+    }
+
+    private void Update()
+    {
+        running = !Input.anyKeyDown;
+    }
+
+    private IEnumerator LifeAnimator()
+    {
+        while (running)
+        {
+            yield return new WaitForSeconds(TimeStep);
+            grid = grid.NextGrid();
+            RenderGrid(grid);
+        }
+        Application.Quit();
+    }
+
+    private void RenderGrid(GameGrid gameGrid)
+    {
+    }    
+}
